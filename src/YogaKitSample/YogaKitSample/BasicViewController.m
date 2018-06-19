937b114f07e1c4c9dfd0b5e381bc259574c33ce6
Joe McBride <joe@uicraftsman.com>
2018-06-18 23:36:09 -0700

    Grow

--- src/YogaKitSample/YogaKitSample/BasicViewController.m
+++ src/YogaKitSample/YogaKitSample/BasicViewController.m
@@ -39,18 +39,20 @@
     child1.name = @"child 1";
     child1.backgroundColor = [NSColor greenColor];
     child1.yoga.isEnabled = YES;
-    child1.yoga.position = YGPositionTypeRelative;
+//    child1.yoga.position = YGPositionTypeRelative;
     child1.yoga.width = YGPointValue(100);
-    child1.yoga.height = YGPointValue(100);
+    child1.yoga.height = YGPointValue(30);
     child1.yoga.flexGrow = 1;
+    child1.yoga.flexShrink = 1;
+    child1.yoga.flexBasis = YGValueAuto;
 
     MyView *child2 = [MyView new];
     child2.name = @"child 2";
     child2.backgroundColor = [NSColor blueColor];
     child2.yoga.isEnabled = YES;
-    child2.yoga.position = YGPositionTypeRelative;
+//    child2.yoga.position = YGPositionTypeRelative;
     child2.yoga.width = YGPointValue(100);
-    child2.yoga.height = YGPointValue(100);
+    child2.yoga.height = YGPointValue(30);
     child2.yoga.flexGrow = 1;
 //    child2.yoga.margin = YGPointValue(10.0);
 
@@ -58,11 +60,10 @@
     child3.name = @"child 3";
     child3.backgroundColor = [NSColor purpleColor];
     child3.yoga.isEnabled = YES;
-//    child3.yoga.position = YGPositionTypeAbsolute;
-    child3.yoga.isIncludedInLayout = YES;
-    child3.yoga.width = YGPointValue(50);
-    child3.yoga.height = YGPointValue(50);
-    child2.yoga.flexGrow = 2;
+//    child3.yoga.position = YGPositionTypeRelative;
+    child3.yoga.width = YGPointValue(100);
+    child3.yoga.height = YGPointValue(30);
+    child3.yoga.flexGrow = 1;
 //    child3.frame = (CGRect) {
 //        .origin = {
 //            .x = 0,
